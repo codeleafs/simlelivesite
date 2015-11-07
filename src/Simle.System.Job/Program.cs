472c5b65c9c496232ba19eef927a2ea792d72c61
xuejp <xuejp@outlook.com>
2015-11-08 00:50:49 +0800

    Adjust project information

---
+++ src/Simle.System.Job/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simle.System.Job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyncData syncData = new SyncData();
+            syncData.Run();
+        }
+    }
+}
---
+++ src/Simle.System.Job/SyncData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+using System.Threading;
+
+namespace Simle.System.Job
+{
+    class SyncData
+    {
+        public void Run()
+        {
+            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            IScheduler scheduler = schedulerFactory.GetScheduler();
+
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<SimpleJob>()
+                 .WithIdentity("JobName", "GroupName")
+                 .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("TriggerName", "GroupName")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(1)
+                    .RepeatForever())
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+
+            Thread.Sleep(10000);
+            scheduler.Shutdown();
+        }
+    }
+
+    class SimpleJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
