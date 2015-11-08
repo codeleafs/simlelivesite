81300973fd79b01725f185fca436eaf29ecc571b
xuejp <xuejp@outlook.com>
2015-11-09 00:10:17 +0800

    Add serializer

---
+++ src/Simle.System.Kit/Serialization/BinarySerializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simle.System.Kit.Serialization
+{
+    public class BinarySerializer
+    {
+        public MemoryStream Serialize(object obj)
+        {
+            // 建立一个继承于Stream的类型的一个对象（可以是FileStream,FileStream等），该对象定义了要将序列化好的字节放到哪里。
+            MemoryStream stream = new MemoryStream();
+
+            // 继承于IFormatter的类型的对象，定义了如何去序列化一个对象。
+            // 格式化器参考对每个对象的类型进行描述的元数据，从而决定如何序列化对象，序列化时利用反射来查看每个对象的类型中都有哪些实例字段。
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, obj);
+
+            return stream;
+        }
+
+        public T Deserializer<T>(Stream stream)
+        {
+            // 从流的起始位置进行序列化
+            stream.Position = 0;
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            return (T)formatter.Deserialize(stream);
+        }
+    }
+}
---
+++ test/Simle.System.Kit.Tests/Serialization/BinarySerializerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simle.System.Kit.Serialization;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Simle.System.Kit.Tests.Serialization
+{
+    [TestClass]
+    public class BinarySerializerTest
+    {
+        [TestMethod]
+        public void SerializeTest()
+        {
+            BinarySerializer serializer = new BinarySerializer();
+            MemoryStream stream = serializer.Serialize(new List<string>() { "f", "s" });
+
+            Assert.IsNotNull(stream);
+        }
+
+        [TestMethod]
+        public void DeserializeTest()
+        {
+            BinarySerializer serializer = new BinarySerializer();
+            var data = new List<string>() { "f", "s" };
+            MemoryStream stream = serializer.Serialize(data);
+            var obj2 = serializer.Deserializer<List<string>>(stream);
+
+            Assert.AreEqual(data[0], obj2[0]);
+        }
+    }
+}
