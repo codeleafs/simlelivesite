bbb012f03e84ba4a4717ae1cb2f58752993de65b
xuejp <xuejp@outlook.com>
2015-11-11 23:05:05 +0800

    Add unit test of serializer

--- test/Simle.System.Kit.Tests/Serialization/BinarySerializerTest.cs
+++ test/Simle.System.Kit.Tests/Serialization/BinarySerializerTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Simle.System.Kit.Serialization;
 using System.IO;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Simle.System.Kit.Tests.Serialization
 {
@@ -28,5 +29,35 @@ namespace Simle.System.Kit.Tests.Serialization
 
             Assert.AreEqual(data[0], obj2[0]);
         }
+
+        [TestMethod]
+        public void SerializableTest()
+        {
+            BinarySerializer serializer = new BinarySerializer();
+
+            SerializableObject obj1 = new SerializableObject() { Name = "test" };
+            serializer.Serialize(obj1);
+
+            try
+            {
+                NonSerializableObject obj2 = new NonSerializableObject() { Name = "test" };
+                serializer.Serialize(obj2);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(SerializationException));
+            }
+        }
+    }
+
+    [Serializable]
+    public class SerializableObject
+    {
+        public string Name { get; set; }
+    }
+
+    public class NonSerializableObject
+    {
+        public string Name { get; set; }
     }
 }
